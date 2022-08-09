79d914dac19d47f1894e2d89b3b99842330f81de
Matej Kerekrety <matejkerekrety@datavant.com>
2022-08-09 11:08:54 +0100

    allow 0, 1

--- algebra.R
+++ algebra.R
@@ -13,7 +13,7 @@ divide <- function(a, b) {
 
 fermats_little_theorem <- function(a, p) {
   primes = c(2,3,5,7,11,13,17)
-  if (!p %in% primes) stop("p is not prime")
+  if (!p %in% primes) stop("p is not a prime")
   if (a < 0) stop("a has to be greater than 0")
   
   return((a**p) %% p)
