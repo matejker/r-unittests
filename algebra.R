e00cbea9b0d032a4e8de85bac4e22976cf8e1bdc
Matej Kerekrety <matejkerekrety@datavant.com>
2022-08-09 10:54:49 +0100

    Fermat s little theorem

--- algebra.R
+++ algebra.R
@@ -10,3 +10,13 @@ divide <- function(a, b) {
   if (b == 0) stop("You cannot divide by 0")
   return(a / b)
 }
+
+fermats_little_theorem <- function(a, p) {
+  primes = c(2,3,5,7,11,13,17)
+  if (!p %in% primes) stop("p is not prime")
+  if (a < 1) stop("a has to be greater than 1")
+  
+  return((a**p) %% p)
+}
+
+fermats_little_theorem(7, 5)
